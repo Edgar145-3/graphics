81065391cf3851a6d574dd153b6fd77617f784e3
Edgar145-3 <cere9590@gmail.com>
2021-10-14 22:29:58 -0500

    Cambios realizados
    BarraOctagono

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,8 +4,8 @@ let graphics;
 canvas = document.getElementById('circlechart');
 graphics = canvas.getContext('2d');
 
-graphics.lineWidth = 8;
-graphics.strokeStyle = "#11111";
+//graphics.lineWidth = 8;
+//ssssgraphics.strokeStyle = "#11111";
 const miCanvas = new CanvasLocal(graphics, canvas);
 
 miCanvas.paint();
